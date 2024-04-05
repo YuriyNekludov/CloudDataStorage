b872525c89a8117401935fd104aff9fc5e20de4d
YuriyNekludov <pacan199@gmail.com>
2024-04-05 20:29:05 +0300

    first commit: create .env-file, add into docker-compose mysql-image, configure application.yaml

---
+++ src/main/java/edu/spring/clouddatastorage/CloudDataStorageApplication.java
@@ -0,0 +1,13 @@
+package edu.spring.clouddatastorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDataStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDataStorageApplication.class, args);
+    }
+
+}
